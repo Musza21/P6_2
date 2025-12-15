8c32a43ca5e02bab5560bd7254bb1086d56440ad
Musza21 <patryk.musza@wp.pl>
2025-12-15 10:02:11 +0100

    P6_4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,3 @@
-/* 
-Program sprawdza poprawność wpisywanego imienia. W przypadku wystąpienia spacji w imieniu, funkcja wyrzuca zdefiniowany wyjątek WrongStudentName, który jest wyłapywany w pętli głównej Commit6_0.
-Poniższe zadania będą się sprowadzały do modyfikacji bazowego kodu. Proces modyfikacji ogólnie może wyglądać następująco:
-• Ustalenie jaki błąd chcę się sprawdzić i wyłapać.
-• Decyzja, czy użyje się własnej klasy wyjątku, czy wykorzysta już istniejące (np. Exception, IOException).
-• Napisanie kodu sprawdzającego daną funkcjonalność. W przypadku warunku błędu wyrzucany będzie wyjątek: throw new WrongStudentName().
-• W definicji funkcji, która zawiera kod wyrzucania wyjątku dopisuje się daną nazwę wyjątku, np. public static String ReadName() throws WrongStudentName.
-• We wszystkich funkcjach, które wywołują powyższą funkcję także należy dopisać, że one wyrzucają ten wyjątek – inaczej program się nie skompiluje.
-• W pętli głównej, w main’ie, w zdefiniowanym już try-catch dopisuje się Nazwę wyjątku i go obsługuje, np. wypisuje w konsoli co się stało.
-*/
-
-//Commit6_1. Na podstawie analogii do wyjątku WrongStudentName utwórz i obsłuż wyjątki WrongAge oraz WrongDateOfBirth. 
-//Niepoprawny wiek – gdy jest mniejszy od 0 lub większy niż 100. Niepoprawna data urodzenia – gdy nie jest zapisana w formacie DD-MM-YYYY, np. 28-02-2023.
 import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -32,7 +19,7 @@ class Main {
                     case 3: exercise3(); break;
                     case 0: return;
                     default:
-                        System.out.println("Błędny wybór menu!");
+                        System.out.println("Błędny wybór menu Zakres: 0–3");
                 }
             } catch(InputMismatchException e) {
                 System.out.println("Błędny format: litera zamiast cyfry!");
